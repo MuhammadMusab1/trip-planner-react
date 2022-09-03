6ca03d68054f271eb149c5ca77c44bf7b988e8f8
musab <yaqoobmusab03@gmail.com>
2022-09-03 12:40:30 -0500

    add html and css for website

--- src/App.js
+++ src/App.js
@@ -1,7 +1,50 @@
 import "./App.css";
 
 function App() {
-  return <h1>Hello</h1>;
+  return (
+    <div className="grid-wrapper">
+      <div className="origin-container">
+        <form className="origin-form">
+          <input placeholder="Find a starting location" type="text" />
+        </form>
+
+        <ul className="origins">
+          <li data-long="-97.1500642" data-lat="49.7959303" class="">
+            <div class="name">Allegheny Drive</div>
+            <div>Winnipeg</div>
+          </li>
+        </ul>
+      </div>
+      <div className="destination-container">
+        <form className="destination-form">
+          <input placeholder="Choose your Destination" type="text" />
+        </form>
+
+        <ul className="destinations">
+          <li data-long="-97.1500642" data-lat="49.7959303" class="selected">
+            <div class="name">Allegheny Drive</div>
+            <div>Winnipeg</div>
+          </li>
+          <li data-long="-97.1500642" data-lat="49.7959303" class="">
+            <div class="name">Allegheny Drive</div>
+            <div>Winnipeg</div>
+          </li>
+        </ul>
+      </div>
+      <div className="button-container">
+        <button className="plan-trip">Plan My Trip</button>
+      </div>
+      <div className="bus-container">
+        <div className="recommended">
+          <ul className="my-trip"></ul>
+        </div>
+
+        <div className="alternative">
+          <ul className="alt-trip"></ul>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
